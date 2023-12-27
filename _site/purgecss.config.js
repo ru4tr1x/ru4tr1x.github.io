6d7d1b41f659291c79e429ee3acf2e9ed68e8838
ru4tr1x <63038758+ru4tr1x@users.noreply.github.com>
2023-12-27 17:00:47 +0000

    big update in _site out of gitignore

---
+++ _site/purgecss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./_site/*.html", // This will match all HTML files in the _site directory
+        "./_site/*.js"    // This will match all JS files in the _site directory
+        // Add other patterns or specific files as needed
+    ],
+    css: [
+        "./_site/resources/css/style.css",
+        "./_site/resources/css/fcp.css",
+        "./_site/resources/css/blog.css",
+        "./_site/resources/css/page-nav.css",
+        "./_site/resources/css/page-nav-2.css"
+        // Add other CSS files or patterns as needed
+    ],
+    // Add any other options you might need here
+};
--- purgecss.config.js
+++ purgecss.config.js
@@ -5,11 +5,11 @@ module.exports = {
         // Add other patterns or specific files as needed
     ],
     css: [
-        "./_site/resources/style.css",
-        "./_site/resources/fcp.css",
-        "./_site/resources/blog.css",
-        "./_site/resources/page-nav.css",
-        "./_site/resources/page-nav-2.css"
+        "./_site/resources/css/style.css",
+        "./_site/resources/css/fcp.css",
+        "./_site/resources/css/blog.css",
+        "./_site/resources/css/page-nav.css",
+        "./_site/resources/css/page-nav-2.css"
         // Add other CSS files or patterns as needed
     ],
     // Add any other options you might need here

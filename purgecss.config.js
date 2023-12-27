65af40448dc46b4bbab48a9fe6f53fce8be89ca5
ru4tr1x <63038758+ru4tr1x@users.noreply.github.com>
2023-12-27 16:26:53 +0000

    purge css

---
+++ purgecss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./_site/*.html", // This will match all HTML files in the _site directory
+        "./_site/*.js"    // This will match all JS files in the _site directory
+        // Add other patterns or specific files as needed
+    ],
+    css: [
+        "./_site/resources/style.css",
+        "./_site/resources/fcp.css",
+        "./_site/resources/blog.css",
+        "./_site/resources/page-nav.css",
+        "./_site/resources/page-nav-2.css"
+        // Add other CSS files or patterns as needed
+    ],
+    // Add any other options you might need here
+};

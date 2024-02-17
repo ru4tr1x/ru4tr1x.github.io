202441140565bb6c8d4c91644e18822f2c3fdc44
ru4tr1x <63038758+ru4tr1x@users.noreply.github.com>
2024-02-17 16:38:45 +0000

    commit all

---
+++ assets/js/lightgal.js
@@ -0,0 +1,9 @@
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  lightGallery(document.getElementById('lightgallery'), {
+    selector: 'a',
+    fullscreen: true, // Assuming these options are needed; they might not be necessary
+    share: true      // if plugins auto-register and these options default to true.
+  })
+});
+</script>
